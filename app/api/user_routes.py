ff572052b0d2fe33da37897e908b66f3c302705a
Ujjal Banik <ub59510@gmail.com>
2026-02-18 09:22:31 +0530

    Added Admin Authorization Rules.

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import Session
 from app.schemas.user import UserCreate, UserResponse
 from app.services.user_service import register_user
 from app.db.database import get_db
-from app.core.dependencies import get_current_user
+from app.core.dependencies import get_current_user, get_current_admin
 from app.models.user import User
+from typing import List
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -14,7 +15,15 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
     created_user = register_user(db, user.email, user.password)
     return UserResponse.model_validate(created_user)
 
+
 @router.get("/me", response_model=UserResponse)
 def read_current_user(current_user: User = Depends(get_current_user)):
     return current_user
 
+
+@router.get("/", response_model=List[UserResponse])
+def get_all_users(
+    db: Session = Depends(get_db),
+    admin: User = Depends(get_current_admin)
+):
+    return db.query(User).all()
--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -25,3 +25,11 @@ def get_current_user(
         )
 
     return user
+
+def get_current_admin(current_user: User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin privileges required"
+        )
+    return current_user
